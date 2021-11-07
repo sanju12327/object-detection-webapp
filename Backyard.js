cd161228aec876474df72c2e038e47b3a9c84eeb
sanju12327 <80778249+sanju12327@users.noreply.github.com>
2021-11-07 12:32:46 -0800

    Add files via upload

---
+++ Backyard.js
@@ -0,0 +1,42 @@
+img="";
+status="";
+
+function preload()
+{
+img = loadImage("Backyard.png");
+}
+
+function setup()
+{
+canvas = createCanvas(900,600);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.log(error);
+    } else
+{
+    console.log(results);
+}
+}
+
+function draw()
+{
+    image(img, 0, 0, 900, 600);
+}
+
+function back()
+{
+    window.location = "Index.html";
+}
